d7ca80b2bb24b89f27bb586d4113e7235092046b
João Paulo <joaowais@gmail.com>
2024-05-07 22:02:40 -0300

    Exerccio01

---
+++ src/vetores/exercicios/exercicio01/application/Program.java
@@ -0,0 +1,30 @@
+package vetores.exercicios.exercicio01.application;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Quandos números você vai digitar?: ");
+		int n = sc.nextInt();
+		
+		int[] vect = new int[n];
+
+		for(int i = 0; i < vect.length; i += 1) {
+			System.out.print("Digite um número: ");
+			vect[i] = sc.nextInt();
+		}
+		
+		System.out.println("NUMEROS NEGATIVOS: ");
+		for(int i = 0; i < vect.length; i += 1) {
+			if (vect[i] < 0) {
+				System.out.println(vect[i]);
+			}
+		}
+		sc.close();
+
+	}
+
+}
