a5b2717344d740455434adaa84b003d6f1705283
João Paulo <joaowais@gmail.com>
2024-05-30 21:24:38 -0300

    Exemplo04 de Datas

---
+++ src/data_hora/app/Exemplo04.java
@@ -0,0 +1,47 @@
+package data_hora.app;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+public class Exemplo04 {
+
+	public static void main(String[] args) {
+		
+		LocalDate d04 = LocalDate.parse("2022-07-20");
+		LocalDateTime d05 = LocalDateTime.parse("2022-07-20T01:30:26");
+		Instant d06 = Instant.parse("2022-07-20T01:30:26Z");
+		
+		LocalDate pastWeekLocalDate = d04.minusWeeks(1);
+		LocalDate nextWeekLocalDate = d04.plusDays(7);
+		LocalDate nextYearLocalDate = d04.plusYears(1);
+		
+		LocalDateTime pastWeekLocalDateTime = d05.minusDays(7);
+		LocalDateTime nextWeekLocalDateTime = d05.plusDays(7);
+		
+		Instant pastWeekInstant = d06.minus(7, ChronoUnit.DAYS);
+		Instant nextWeekInstant = d06.plus(7, ChronoUnit.DAYS);
+		
+		Duration t1 = Duration.between(pastWeekLocalDateTime, d05);
+		Duration t2 = Duration.between(pastWeekLocalDateTime, d04.atStartOfDay());
+		Duration t3 = Duration.between(pastWeekInstant, d06);
+		
+		System.out.println("pastWeekLocalDate " + pastWeekLocalDate);
+		System.out.println("nextWeekLocalDate " + nextWeekLocalDate);
+		System.out.println("nextYearLocalDate " + nextYearLocalDate);
+		System.out.println("========================================");
+		System.out.println("pastWeekLocalDateTime " + pastWeekLocalDateTime);
+		System.out.println("nextWeekLocalDateTime " + nextWeekLocalDateTime);
+		System.out.println("========================================");
+		System.out.println("pastWeekInstant " + pastWeekInstant);
+		System.out.println("nextWeekInstant " + nextWeekInstant);
+		System.out.println("========================================");
+		System.out.println("t1 " + t1.toDays());
+		System.out.println("t2 " + t2.toDays());
+		System.out.println("t3 " + t3.toDays());
+
+	}
+
+}
